05f9383cd7ee08dfcbaaf89b055c6df4684e5f90
Retus001 <retus001@gmail.com>
2020-01-13 14:06:47 -0600

    Pokeinicio

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    public SpriteRenderer renderer;
+    public Sprite triste, feliz;
+    public enum estadoAnimal { Idle, triste, llorando, enojado, feliz};
+
+    private estadoAnimal miEstado;
+    private bool owned;
+    private bool petHouse;
+
+    private float minHambre,minFelicidad, maxHambre,maxFelicidad;
+    private float felicidad, hambre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        owned = false;
+        miEstado = estadoAnimal.triste;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (owned)
+        {
+            hambre -= 0.5f;
+            //felicidad -= 0.5f;
+            if (petHouse)
+            {
+                felicidad += 0.5f;
+            }
+            if (hambre < minHambre)
+            {
+                felicidad -= 1.3f;
+            }
+        }
+    }
+    private void changeEstado(estadoAnimal _estado)
+    {
+        miEstado = _estado;
+        switch (_estado)
+        {
+            case estadoAnimal.feliz:
+                renderer.sprite = feliz;
+                break;
+            case estadoAnimal.llorando:
+                renderer.sprite = triste;
+                break;
+        }
+    }
+    public bool isOwned()
+    {
+        return owned;
+    }
+    public void setAdoptado(Vector2 _posicion)
+    {
+        owned = true;
+        changeEstado(estadoAnimal.feliz);
+        transform.position = _posicion;
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float movementSpeed=2.0f;
+    public Rigidbody2D rigi;
+
+    private Vector2 movActual;
+    public Vector2 posicionAdoptado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionAdoptado = new Vector2(-4.7f, 0.8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movActual.x = Input.GetAxis("Horizontal");
+        movActual.y = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rigi.MovePosition(rigi.position + (movActual * movementSpeed * Time.fixedDeltaTime));
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Puchurrae a la E para adoptarme");
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && collision.transform.root.GetComponent<Animal>() && !collision.transform.root.GetComponent<Animal>().isOwned())
+        {
+            Debug.Log("Adoptando");
+            collision.transform.root.GetComponent<Animal>().setAdoptado(posicionAdoptado);
+        }
+    }
+}
