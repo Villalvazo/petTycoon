ebcbe013743adaa410f7356b45da8d24340bf454
Retus001 <retus001@gmail.com>
2020-01-20 13:10:20 -0600

    Asi es
    Confirmo

---
+++ Assets/Scripts/Puerta.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour
+{
+    public GameObject tileActivable;
+    private bool activo;
+
+    private void Start()
+    {
+        activo = true;
+    }
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        activo = !activo;
+        tileActivable.SetActive(activo);
+    }
+}
